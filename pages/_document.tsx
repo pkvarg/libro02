04abf87069bb58cf0e5c530193e73089a81dc18b
peter varga <peter.varga@staffino.com>
2025-08-16 20:20:04 +0200

    unami

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script defer src="https://umami-p00gs00gwcwo00s4k4c4kgg8.pictusweb.com/script.js" data-website-id="2ad99828-4e2f-4bcd-acbc-ed2e0ce40322"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
